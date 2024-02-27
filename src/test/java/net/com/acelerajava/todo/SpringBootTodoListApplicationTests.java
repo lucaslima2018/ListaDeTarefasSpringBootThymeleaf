7e4134c33a1c9a667077860cfc29ced48e46ebe1
lucaslima2018 <33165385+lucaslima2018@users.noreply.github.com>
2024-02-26 21:47:27 -0300

    Add files via upload

---
+++ src/test/java/net/com/acelerajava/todo/SpringBootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package net.com.acelerajava.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
